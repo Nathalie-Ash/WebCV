c86721a5c35a3421c8fc8fab7e2df7e54725f744
NATHALIE\hp <elashkarnathalie@gmail.com>
2023-11-18 17:01:55 +0200

    Fixed text position of buttons

--- pages/contact.php
+++ pages/contact.php
@@ -9,7 +9,7 @@
 <html>
 
 <head>
-    <link rel="stylesheet" href=".././cv.css">
+    <link rel="stylesheet" href="../cv.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href ="https://use.fontawesome.com/releases/v5.0.8/css/all.css">
 
--- pages/index.php
+++ pages/index.php
@@ -8,7 +8,7 @@ session_destroy();
 <html>
 
 <head>
-    <link rel="stylesheet" href=".././cv.css">
+    <link rel="stylesheet" href="../cv.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 
